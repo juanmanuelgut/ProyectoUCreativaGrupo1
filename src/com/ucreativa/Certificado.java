8469399aae5598ad53b48f0a38dd3a4e4aaf4dfd
Juan Gutierrez <juanmanuelgut@gmail.com>
2021-02-15 20:49:47 -0600

    adding classes

---
+++ src/com/ucreativa/Certificado.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.ucreativa;
+
+/**
+ * @author juangutierrez
+ *
+ */
+public class Certificado {
+
+	/**
+	 * 
+	 */
+	public Certificado() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/com/ucreativa/Cuenta.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.ucreativa;
+
+/**
+ * @author juangutierrez
+ *
+ */
+public class Cuenta {
+
+	/**
+	 * 
+	 */
+	public Cuenta() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
